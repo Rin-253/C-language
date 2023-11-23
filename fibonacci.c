a6fa2fe8aa09f6e9a110eddca46b30b80ab44133
Rin <lucyrose1531@gmail.com>
2023-11-23 17:40:56 +0700

    nhap n sau do in day fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include"stdio.h"
+#include"conio.h"
+int f(int n);
+
+int f(int n)
+{
+  if(n<0){ return -1;
+  }
+  else
+  	if(n==0||n==1) {return n;
+  }
+  else{
+  	return f(n-1)+f(n-2);
+  }
+}
+
+int main()
+{
+  int n;
+  printf("nhap n=");scanf("%d",&n);
+  printf("so hang thu %d cua day la : %d ",n,f(n));
+  getch();
+  return 0;
+}
+
+
